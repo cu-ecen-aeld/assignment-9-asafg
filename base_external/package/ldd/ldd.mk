f52db6cfcadd71dae244c5998f31151215c2529a
Asaf Gery <asaf.gery@gmail.com>
2024-03-06 22:56:15 +0200

    Assingment 7 part 2 buildroot first try...

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# LDD
+#
+################################################################################
+
+LDD_VERSION = '369466085ce1e950c7f5fd308c26f8a6cc569b1f'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignments-7-asafg.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_LICENSE = 'Dual BSD/GPL'
+LDD_LICENSE_FILES = COPYING
+LDD_MODULE_SUBDIRS = "misc-modules scull"
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
