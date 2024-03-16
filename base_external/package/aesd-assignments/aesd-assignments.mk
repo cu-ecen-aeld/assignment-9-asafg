8c5854160f44336872ee2730d29f515740a7bcc9
Asaf Gery <asaf.gery@gmail.com>
2024-03-17 00:36:56 +0200

    Updated aesd packages' version to assignmet 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AESD_ASSIGNMENTS_VERSION = '3d22434b8fc872f59cba8f6abb7cccda7aeb32c3'
+AESD_ASSIGNMENTS_VERSION = 'e6f5b0016ecbee219320c03b9e671f3ffbe5085f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
